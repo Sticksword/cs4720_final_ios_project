3b81e11e2151b78dd222e5f7ee39c23446d8bb54
Jordan <jjl4mb@virginia.edu>
2016-04-27 21:15:02 -0400

    Starte on descriptions page

---
+++ Places We Have Been/DetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.swift
+//  Places We Have Been
+//
+//  Created by Jordan Lu on 4/27/16.
+//  Copyright © 2016 Michael Chen. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController : UIViewController {
+    
+}
